1ad7c850ea51eb59999cb08d831b27af5dc68325
Jimmy Quach <jminquach@gmail.com>
2021-08-27 03:03:22 -0700

    Update VideoSortControls.tsx
    Reload page instead

--- ClientApp/src/components/VideoSortControls.tsx
+++ ClientApp/src/components/VideoSortControls.tsx
@@ -48,7 +48,7 @@ export const VideoSortControls: React.FC<Props> = ({gameList, game, sortBy, size
       <span className="mt-0.5 inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-500 active:bg-gray-50 active:text-gray-800">
         {formatBytes(size)}
       </span>
-      <Button onClick={() => postMessage('RetrieveVideos', {game: game, sortBy: sortBy})} icon={
+      <Button onClick={() => window.location.reload()} icon={
         <svg className="mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" >
           <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
           <path fillRule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
