c8d6af150e6ef5a2ba6144ab59a6f43bc5506486
Jimmy Quach <jminquach@gmail.com>
2021-09-02 04:47:42 -0700

    Grid/Row view controls

--- ClientApp/src/components/VideoSortControls.tsx
+++ ClientApp/src/components/VideoSortControls.tsx
@@ -9,9 +9,10 @@ interface Props {
   game: string;
   sortBy: string;
   size: number;
+  setVideoView: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const VideoSortControls: React.FC<Props> = ({gameList, game, sortBy, size}) => {
+export const VideoSortControls: React.FC<Props> = ({gameList, game, sortBy, size, setVideoView}) => {
   function getGamesDDM() {
     let items = [
       {name: 'All Games', onClick: () => {postMessage('RetrieveVideos', {game: 'All Games', sortBy: sortBy})}}
@@ -39,12 +40,12 @@ export const VideoSortControls: React.FC<Props> = ({gameList, game, sortBy, size
         <svg className="mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
         </svg>
-      }/>
+      } onClick={() => {setVideoView('grid')}}/>
       <Button icon={
         <svg className="mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z"/>
         </svg>
-      }/>
+      } onClick={() => {setVideoView('row')}}/>
       <span className="mt-0.5 inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-500 active:bg-gray-50 active:text-gray-800">
         {formatBytes(size)}
       </span>
--- ClientApp/src/pages/VideosPage.tsx
+++ ClientApp/src/pages/VideosPage.tsx
@@ -17,6 +17,7 @@ interface Props {
 export const VideosPage: React.FC<Props> = ({videoType, gameList, game, sortBy, videos, size}) => {
   const [checkedVideos, setCheckedVideos] = useState(Array(videos.length).fill(false));
   const [checkedLength, setCheckedLength] = useState(0);
+  const [videoView, setVideoView] = useState('grid');
 
   function onVideoSelected(e: React.ChangeEvent<HTMLInputElement>, index:number) {
     console.log((e.target as HTMLInputElement).checked);
@@ -43,7 +44,7 @@ export const VideosPage: React.FC<Props> = ({videoType, gameList, game, sortBy, 
     <div className="flex flex-col h-full border-0 border-b"> 
       <div className="pb-4 flex-initial border-0 border-b">
        {videoType}
-       <VideoSortControls gameList={gameList} game={game} sortBy={sortBy} size={size}/>
+       <VideoSortControls gameList={gameList} game={game} sortBy={sortBy} size={size} setVideoView={setVideoView}/>
        {checkedLength > 0 && 
        <VideoDeleteControls length={checkedLength} 
          selectAll={() => {setCheckedVideos(Array(videos.length).fill(true)); setCheckedLength(videos.length);}}
@@ -53,7 +54,12 @@ export const VideosPage: React.FC<Props> = ({videoType, gameList, game, sortBy, 
       <VirtuosoGrid
         totalCount={videos.length}
         overscan={4}
-        listClassName={"gap-8 grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 pr-8 mt-4 mb-4"}
+        listClassName={(
+          videoView === 'grid' ? 
+          "gap-8 grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 pr-8 mt-4 mb-4" 
+          : 
+          "gap-8 grid grid-flow-row grid-cols-1 pr-8 mt-4 mb-4" // TODO, make this look better
+        )}
         itemClassName={"overflow-hidden shadow-lg h-90 md:w-auto cursor-pointer m-auto"}
         itemContent={index => 
           <Card key={videos[index].fileName} 
