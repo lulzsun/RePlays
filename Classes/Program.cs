40f5257329cc37808cb811b4ee5767655383ed0b
Jimmy Quach <jminquach@gmail.com>
2021-11-01 12:35:20 -0700

    Enforce single running instance

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using RePlays.Services;
-using RePlays;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace RePlays {
     public class Program {
@@ -15,6 +17,10 @@ namespace RePlays {
 
         [STAThread]
         public static void Main(string[] args) {
+            if (SingleApplicationDetector.IsRunning()) {
+                return;
+            }
+
             // redirect console output to parent process;
             // must be before any calls to Console.WriteLine()
             string debugArg = "-debug";
@@ -29,6 +35,8 @@ namespace RePlays {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+
+            SingleApplicationDetector.Close();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -36,5 +44,31 @@ namespace RePlays {
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public static class SingleApplicationDetector {
+        public static bool IsRunning() {
+            string guid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+            var semaphoreName = @"Global\" + guid;
+            try {
+                __semaphore = Semaphore.OpenExisting(semaphoreName);
+                 
+                Close();
+                return true;
+            }
+            catch (Exception ex) {
+                __semaphore = new Semaphore(0, 1, semaphoreName);
+                return false;
+            }
+        }
+
+        public static void Close() {
+            if (__semaphore != null) {
+                __semaphore.Close();
+                __semaphore = null;
+            }
+        }
+
+        private static Semaphore __semaphore;
     }
 }
