b848903201f872e02d3d945b5af60fa36e3e6cee
Jimmy Quach <jminquach@gmail.com>
2021-11-02 02:55:17 -0700

    Update single running instance detection
    Simpler, and actually works for asp.net core apps

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,13 +1,12 @@
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using RePlays.Services;
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security.AccessControl;
+using RePlays;
 using System.Threading;
-using System.Windows.Forms;
 
 namespace RePlays {
     public class Program {
@@ -17,9 +16,7 @@ namespace RePlays {
 
         [STAThread]
         public static void Main(string[] args) {
-            if (SingleApplicationDetector.IsRunning()) {
-                return;
-            }
+            const string mutexName = @"Global\RePlays";
 
             // redirect console output to parent process;
             // must be before any calls to Console.WriteLine()
@@ -29,14 +26,18 @@ namespace RePlays {
                 AttachConsole(ATTACH_PARENT_PROCESS);
             }
 
+            var mutex = new Mutex(true, mutexName, out var createdNew);
+            if (!createdNew) {
+                Logger.WriteLine(mutexName + " is already running! Exiting the application.");
+                return;
+            }
+
             CreateHostBuilder(args).Build().RunAsync();
 
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
-
-            SingleApplicationDetector.Close();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -44,31 +45,5 @@ namespace RePlays {
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
-
-    public static class SingleApplicationDetector {
-        public static bool IsRunning() {
-            string guid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
-            var semaphoreName = @"Global\" + guid;
-            try {
-                __semaphore = Semaphore.OpenExisting(semaphoreName);
-                 
-                Close();
-                return true;
-            }
-            catch (Exception ex) {
-                __semaphore = new Semaphore(0, 1, semaphoreName);
-                return false;
-            }
-        }
-
-        public static void Close() {
-            if (__semaphore != null) {
-                __semaphore.Close();
-                __semaphore = null;
-            }
-        }
-
-        private static Semaphore __semaphore;
     }
 }
