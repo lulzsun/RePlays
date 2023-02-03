2a7c2ea6e691e506efd58f946fc6b124524a882c
Segergren <olle.segergren@gmail.com>
2023-02-03 22:20:49 +0100

    Added file limit warning

--- Classes/Uploaders/RePlaysUploader.cs
+++ Classes/Uploaders/RePlaysUploader.cs
@@ -9,11 +9,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using RePlays.Services;
 using RePlays.Utils;
+using SharpCompress.Common;
 using static RePlays.Utils.Functions;
 
 namespace RePlays.Uploaders {
     public class RePlaysUploader : BaseUploader {
-        public class StreamableResult {
+        public class RePlaysResult {
             public string shortcode { get; set; }
             public int status { get; set; }
         }
@@ -25,6 +26,15 @@ namespace RePlays.Uploaders {
                 var credentials = $"{rePlaysSettings.email}:{DecryptString(rePlaysSettings.password)}";
                 var authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authorization);
+                HttpResponseMessage uploadLimitResponse = await httpClient.GetAsync("https://replays.app/v1/method/getuploadlimit.php");
+                int fileLimit = int.Parse(await uploadLimitResponse.Content.ReadAsStringAsync());
+
+                int fileSize = (int)(new FileInfo(file).Length / (1024.0 * 1024.0));
+                if (fileSize > fileLimit)
+                {
+                    WebMessage.DisplayModal("Max file size is " + fileLimit + " MB. Your file is " + fileSize + " MB", "Warning", "warning");
+                    return null;                
+                }
 
                 using (var formDataContent = new MultipartFormDataContent()) {
                     var titleContent = new StringContent(title);
@@ -39,15 +49,26 @@ namespace RePlays.Uploaders {
                     var response = await httpClient.PostAsync("https://replays.app/v1/method/upload.php", formDataContent);
                     var content = response.Content.ReadAsStringAsync().Result;
                     Logger.WriteLine(response.StatusCode.ToString() + " " + content);
-                    var result = JsonSerializer.Deserialize<StreamableResult>(content);
-                    if (result.shortcode != null) {
-                        Process browserProcess = new Process();
-                        browserProcess.StartInfo.UseShellExecute = true;
-                        browserProcess.StartInfo.FileName = "https://replays.app/Video/" + result.shortcode;
-                        browserProcess.Start();
-                        return "https://replays.app/Video/" + result.shortcode;
-                    } else {
-                        throw new NullReferenceException($"shortcode is null, but response is {(int)response.StatusCode}: {response.StatusCode}");
+                    try
+                    {
+                        var result = JsonSerializer.Deserialize<RePlaysResult>(content);
+                        if (result.shortcode != null)
+                        {
+                            Process browserProcess = new Process();
+                            browserProcess.StartInfo.UseShellExecute = true;
+                            browserProcess.StartInfo.FileName = "https://replays.app/Video/" + result.shortcode;
+                            browserProcess.Start();
+                            return "https://replays.app/Video/" + result.shortcode;
+                        }
+                        else
+                        {
+                            throw new NullReferenceException($"shortcode is null, but response is {(int)response.StatusCode}: {response.StatusCode}");
+                        }
+                    }
+                    catch
+                    {
+                        WebMessage.DisplayModal("An unexpected error occured.", "Warning", "warning");
+                        return null;
                     }
                 }
             }
