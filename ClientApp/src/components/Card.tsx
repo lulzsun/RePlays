c5aa83c3c6ba19dd7cbdd5cb206ed13578600b47
Jimmy Quach <jminquach@gmail.com>
2023-07-03 05:21:16 -0700

    fix: Teamfight Tactics showing integration info

--- ClientApp/src/components/Card.tsx
+++ ClientApp/src/components/Card.tsx
@@ -68,7 +68,7 @@ export const Card: React.FC<Props> = ({date=Date.now().toString(), game="Game Un
           <span className="absolute z-40 bottom-1 right-1 py-0.5 px-2 rounded-full text-xs font-normal" style={{backgroundColor: `rgba(0, 0, 0, 0.5)`}}>
             {duration > 3600 ? new Date(duration * 1000).toISOString().substr(11, 8).replace(/^0+/, '') : new Date(duration * 1000).toISOString().substring(14, 19)}
           </span>
-          {game === "League of Legends" && videoType === "Sessions" && champion && (
+          {game === "League of Legends" && videoType === "Sessions" && champion && champion !== "TFTChampion" && (
             <span className="absolute z-40 bottom-1 left-1 text-xs font-normal flex items-center">
               <img className="border border-black -mr-4 z-40 rounded-full" src={`https://ddragon.leagueoflegends.com/cdn/13.13.1/img/champion/${champion}.png`} style={{ width: "20px" }} alt={champion}/>
               <p className="py-0.5 pl-5 rounded-full p-2" style={{ backgroundColor: `rgba(0, 0, 0, 0.5)` }}>
