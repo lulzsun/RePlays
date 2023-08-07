86582805e964b4060f35d9bb3c9ab05c63898a5c
Jimmy Quach <jminquach@gmail.com>
2023-08-07 16:26:22 -0700

    Fix keybind editing button styles

--- ClientApp/src/components/KeybindSelector.tsx
+++ ClientApp/src/components/KeybindSelector.tsx
@@ -28,14 +28,17 @@ export const KeybindSelector: React.FC<Props> = ({id, keybind}) => {
         ]}/>
       </div>
       <div className="flex-col w-1/2">
-        <span>Keybind</span>
-        <div className={`cursor-pointer bg-white dark:bg-gray-900 text-gray-700 dark:text-white border-gray-500 dark:border-gray-400 hover:text-gray-700 focus:border-blue-300
-          px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-md focus:outline-none focus:shadow-outline-blue`}
-          onClick={(e) => { e.currentTarget.style.borderColor = "#efaf2b" }} 
-          onFocus={(e) => { e.currentTarget.style.borderColor = "rgba(156, 163, 175, var(--tw-border-opacity))" }}
+        <div>Keybind</div>
+        <button className={`bg-white text-gray-700 border-gray-500 hover:text-white hover:bg-gray-900 hover:border-gray-900 focus:border-yellow-600 focus:bg-yellow-600 focus:text-white
+        dark:bg-gray-900 dark:text-gray-400 dark:border-gray-400 dark:hover:text-gray-300 dark:hover:border-gray-300 dark:focus:border-yellow-600 dark:focus:text-yellow-600
+          group w-full px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-md`}
+          onBlur={(e) => { console.log("I'm out of focus") }} 
+          onFocus={(e) => { console.log("I'm in focus") }}
         >
-          {keybind ? keybind.join("+") : "None"}
-        </div>
+          <a>
+            {keybind ? keybind.join("+") : "None"}
+          </a>
+        </button>
       </div>
       <div className="flex-col w-auto">
         <span>Enable</span>
