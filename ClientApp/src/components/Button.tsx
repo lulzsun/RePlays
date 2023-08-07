6151b6775ac7885d8b45a422cfb4c2c883ecad7d
Jimmy Quach <jminquach@gmail.com>
2023-08-07 16:05:55 -0700

    fix(style): Minor UI styling/visual inconsistencies

--- ClientApp/src/components/Button.tsx
+++ ClientApp/src/components/Button.tsx
@@ -10,8 +10,9 @@ interface Props {
 export const Button: React.FC<Props> = ({onClick, text, icon, width="full"}) => {
 	return (
     <div className="relative inline-block text-left dropdown">
-      <button className={`bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-400 border-gray-500 dark:border-gray-400 hover:text-gray-700
-      inline-flex whitespace-nowrap justify-center w-${width} h-full px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-md focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800`}
+      <button className={`bg-white text-gray-700 border-gray-500 hover:text-white hover:bg-gray-900 hover:border-gray-900
+      dark:bg-gray-900 dark:text-gray-400 dark:border-gray-400 dark:hover:text-gray-300 dark:hover:border-gray-300
+      inline-flex whitespace-nowrap justify-center w-${width} h-full px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-md`}
         type="button" onClick={onClick}>
           {text}
           {icon && icon}
