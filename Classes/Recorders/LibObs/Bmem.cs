875c44c5818a5db1004b1d9e35e77daa7933c837
Jimmy Quach <jminquach@gmail.com>
2023-12-04 18:23:58 -0800

    chore: Remove LibObs in prep for libobs-sharp submodule

--- Classes/Recorders/LibObs/Bmem.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace obs_net {
-    public partial class Obs {
-        [DllImport(importLibrary, CallingConvention = importCall)]
-        public static extern long bnum_allocs();
-    }
-}
