lulzsun/RePlays

commit 7efe34ffb61ce77339a14bdc9c0ed26ea1bea1a2

feat: Made bookmark icon clickable. Changed death icon. (#176)

ClientApp/src/components/Bookmark.tsx
@@ -77,7 +77,7 @@ export const Bookmark = forwardRef<HTMLDivElement, Props>(
                   fill: 'rgb(97.647059%,80%,7.843137%)',
                   fillOpacity: 1,
                 }}
-                d='M 0.910156 0.796875 L 14.058594 0.796875 C 14.5625 0.796875 14.96875 1.160156 14.96875 1.605469 L 14.96875 14.691406 C 14.96875 15.136719 14.5625 15.5 14.058594 15.5 L 0.910156 15.5 C 0.40625 15.5 0 15.136719 0 14.691406 L 0 1.605469 C 0 1.160156 0.40625 0.796875 0.910156 0.796875 Z M 0.910156 0.796875 '
+                  d='M 0.925781 0.796875 L 14.070312 0.796875 C 14.578125 0.796875 14.980469 1.160156 14.980469 1.605469 L 14.980469 14.691406 C 14.980469 15.136719 14.578125 15.5 14.070312 15.5 L 0.925781 15.5 C 0.417969 15.5 0.0117188 15.136719 0.0117188 14.691406 L 0.0117188 1.605469 C 0.0117188 1.160156 0.417969 0.796875 0.925781 0.796875 Z M 0.925781 0.796875 '
               />{' '}
               <path
                 style={{
@@ -86,17 +86,16 @@ export const Bookmark = forwardRef<HTMLDivElement, Props>(
                   fill: 'rgb(97.647059%,80%,7.843137%)',
                   fillOpacity: 1,
                 }}
-                d='M 7.320312 28.199219 C 7.4375 28.410156 7.554688 28.410156 7.671875 28.199219 L 14.914062 15.089844 C 15.027344 14.878906 14.96875 14.777344 14.738281 14.777344 L 0.21875 14.777344 C -0.015625 14.777344 -0.0742188 14.878906 0.0429688 15.089844 Z M 7.320312 28.199219 '
+                  d='M 7.332031 28.199219 C 7.449219 28.410156 7.566406 28.410156 7.683594 28.199219 L 14.929688 15.089844 C 15.039062 14.878906 14.980469 14.777344 14.753906 14.777344 L 0.234375 14.777344 C 0 14.777344 -0.0585938 14.878906 0.0585938 15.089844 Z M 7.332031 28.199219 '
               />{' '}
               <path
                 style={{
-                  paddingTop: '1.1rem',
                   stroke: 'none',
                   fillRule: 'nonzero',
                   fill: 'rgb(0%,0%,0%)',
                   fillOpacity: 1,
                 }}
-                d='M1 6.5a6.5 6.5 0 0113 0v3.382a1.5 1.5 0 01-.83 1.342l-1.17.585v.691A2.5 2.5 0 019.5 15h-4A2.5 2.5 0 013 12.5v-.691l-1.17-.585A1.5 1.5 0 011 9.882V6.5zM4.5 6a1.5 1.5 0 100 3 1.5 1.5 0 000-3zm6 0a1.5 1.5 0 100 3 1.5 1.5 0 000-3zM7.146 9.146a.5.5 0 01.708 0l1 1-.708.708-.646-.647-.646.647-.708-.708 1-1z'
+                  d='M 5.394531 1.199219 C 4.113281 1.484375 3.367188 1.800781 2.523438 2.421875 C 1.011719 3.558594 0.214844 5.386719 0.371094 7.414062 C 0.464844 8.582031 1.058594 10.320312 1.277344 10.109375 C 1.355469 10.03125 1.554688 9.59375 1.710938 9.140625 C 1.882812 8.6875 2.101562 8.324219 2.226562 8.324219 C 2.570312 8.324219 2.429688 9.214844 1.898438 10.304688 C 1.632812 10.847656 1.414062 11.46875 1.414062 11.652344 C 1.414062 12.164062 2.101562 12.875 2.976562 13.253906 C 3.664062 13.558594 3.753906 13.648438 3.753906 14.058594 C 3.753906 14.722656 4.238281 15.402344 4.941406 15.75 C 5.832031 16.175781 9.167969 16.175781 10.058594 15.75 C 10.757812 15.402344 11.242188 14.722656 11.242188 14.058594 C 11.242188 13.648438 11.335938 13.558594 12.023438 13.253906 C 12.898438 12.875 13.582031 12.164062 13.582031 11.652344 C 13.582031 11.46875 13.363281 10.847656 13.097656 10.304688 C 12.585938 9.214844 12.429688 8.324219 12.773438 8.324219 C 12.882812 8.324219 13.097656 8.6875 13.253906 9.125 C 13.410156 9.5625 13.613281 10.003906 13.707031 10.09375 C 13.941406 10.320312 14.535156 8.640625 14.628906 7.414062 C 14.847656 4.570312 13.253906 2.285156 10.449219 1.425781 C 9.152344 1.015625 6.671875 0.910156 5.394531 1.199219 Z M 6.078125 8.15625 C 7.640625 8.941406 6.734375 11.425781 4.894531 11.425781 C 3.632812 11.425781 3.101562 10.410156 3.664062 9.09375 C 4.097656 8.082031 5.128906 7.6875 6.078125 8.15625 Z M 10.695312 8.277344 C 11.585938 8.957031 11.835938 10.410156 11.164062 11.046875 C 10.136719 12.058594 8.109375 11.015625 8.125 9.503906 C 8.140625 8.21875 9.667969 7.492188 10.695312 8.277344 Z M 8.046875 12.074219 C 8.265625 12.4375 8.433594 12.847656 8.433594 12.996094 C 8.433594 13.375 7.953125 13.480469 7.71875 13.164062 C 7.5 12.890625 7.5 12.890625 7.28125 13.164062 C 7.046875 13.480469 6.5625 13.375 6.5625 12.996094 C 6.5625 12.664062 7.3125 11.425781 7.5 11.425781 C 7.578125 11.425781 7.828125 11.726562 8.046875 12.074219 Z M 8.046875 12.074219 '
               />{' '}
             </g>{' '}
           </svg>

ClientApp/src/pages/Player.tsx
@@ -65,6 +65,7 @@ export const Player: React.FC<Props> = ({ videos }) => {
 
     function handleOnMouseDown(e: MouseEvent) {
       let element = e.target as HTMLDivElement;
+      element = findBookmarkAncestorOrSelf(element);
 
       // seeker handling
       if (e.button === 0) {
@@ -174,6 +175,24 @@ export const Player: React.FC<Props> = ({ videos }) => {
       }
     }
 
+    // Enables clicking on the bookmark icon.
+    function findBookmarkAncestorOrSelf(element: HTMLDivElement): HTMLDivElement {
+      if (element.hasAttribute('data-index')) {
+        return element;
+      }
+
+      let parentElement: HTMLElement | null = element.parentElement;
+      while (parentElement) {
+        if (parentElement.hasAttribute('data-index')) {
+          return parentElement as HTMLDivElement;
+        }
+        parentElement = parentElement.parentElement;
+      }
+
+      // If no parent with data-index was found, return the original element
+      return element;
+    }
+
     function handleOnMouseUp(e: MouseEvent) {
       seekDragging = false;
       clipResizeDir = '';
@@ -246,7 +265,7 @@ export const Player: React.FC<Props> = ({ videos }) => {
 
     modalCtx?.setData({
       title: 'Upload',
-      context: <UploadModal video={video} game={game} thumb={thumb} />,
+      context: <UploadModal video={video} game={game} thumb={thumb} makePublic={true} />,
       cancel: true,
     });
     modalCtx?.setOpen(true);
@@ -424,27 +443,6 @@ export const Player: React.FC<Props> = ({ videos }) => {
 
   return (
     <div className='h-full flex flex-col'>
-      {(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') &&
-        folder?.includes('videos.replays.app') && (
-          <div className='flex flex-col gap-2 font-medium text-base pb-2'>
-            <span className='font-normal text-sm'>
-              DEV ONLY NOTE: Player has issues that only occur when debugging, it works perfectly
-              fine in production
-            </span>
-            <span className='font-normal text-sm'>
-              see this issue for more details:
-              <a
-                className='cursor-pointer underline pl-2'
-                onClick={(e) => {
-                  postMessage('ShowFolder', 'https://github.com/lulzsun/RePlays/issues/11');
-                }}
-              >
-                https://github.com/lulzsun/RePlays/issues/11
-              </a>
-            </span>
-          </div>
-        )}
-
       <div
         className='relative flex w-full h-full bg-black justify-center cursor-pointer'
         onClick={() => {

commit c8bfc902138a78618dbee4569cfff2c9e9394b88

Fixes issue introduced in #176

ClientApp/src/components/Bookmark.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const Bookmark = forwardRef<HTMLDivElement, Props>(
   ({ id, time = 0, type = BookmarkType.Manual }, ref) => {
-    let className = 'grid grid-flow-col absolute rounded-lg h-full bg-yellow-400';
+    let className = 'grid grid-flow-col absolute rounded-lg h-full bg-yellow-400 bookmark';
     return (
       <div
         ref={ref}

ClientApp/src/pages/Player.tsx
@@ -177,20 +177,8 @@ export const Player: React.FC<Props> = ({ videos }) => {
 
     // Enables clicking on the bookmark icon.
     function findBookmarkAncestorOrSelf(element: HTMLDivElement): HTMLDivElement {
-      if (element.hasAttribute('data-index')) {
-        return element;
-      }
-
-      let parentElement: HTMLElement | null = element.parentElement;
-      while (parentElement) {
-        if (parentElement.hasAttribute('data-index')) {
-          return parentElement as HTMLDivElement;
-        }
-        parentElement = parentElement.parentElement;
-      }
-
-      // If no parent with data-index was found, return the original element
-      return element;
+      const bookmarkElement = element.closest('.bookmark');
+      return bookmarkElement as HTMLDivElement || element;
     }
 
     function handleOnMouseUp(e: MouseEvent) {