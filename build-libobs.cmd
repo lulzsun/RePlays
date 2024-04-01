38fa6a1658ff1b97066cc9e60bd79c22b484ec94
Jimmy Quach <jminquach@gmail.com>
2024-04-01 11:30:11 -0700

    feat: Update OBS from 29.1.1 -> 30.1.1 (#182)
    Major build script changes, some are done to reflect new OBS build steps

--- build-libobs.cmd
+++ build-libobs.cmd
@@ -1,65 +1,85 @@
 @echo off
 
-set OBS_STUDIO_VERSION=29.1.1
-set OBS_DEPS_RELEASE_DATE=2023-04-12
+set "OBS_STUDIO_VERSION=30.1.1"
 
-set BASE_DIR=%CD%
-set OBS_STUDIO_BUILD_DIR=%BASE_DIR%\obs-studio-build
-set OBS_STUDIO_DIR=%OBS_STUDIO_BUILD_DIR%\obs-studio-%OBS_STUDIO_VERSION%
-set WINDOWS_DEPS_DIR=%OBS_STUDIO_BUILD_DIR%\windows-deps
-set OBS_STUDIO_RELEASE_DIR=%OBS_STUDIO_BUILD_DIR%\obs-studio-release
-set OBS_INSTALL_PREFIX=%OBS_STUDIO_DIR%\build
+set "BASE_DIR=%CD%"
+set "OBS_STUDIO_BUILD_DIR=%BASE_DIR%\obs-studio-build"
+set "OBS_STUDIO_DIR=%OBS_STUDIO_BUILD_DIR%\obs-studio-%OBS_STUDIO_VERSION%"
+set "OBS_STUDIO_RELEASE_DIR=%OBS_STUDIO_BUILD_DIR%\obs-studio-release"
+set "OBS_INSTALL_PREFIX=%OBS_STUDIO_DIR%\build"
 
-echo "Building obs-studio"
+echo "Building obs-studio-%OBS_STUDIO_VERSION%"
 mkdir "%OBS_STUDIO_BUILD_DIR%" 2>NUL
 cd "%OBS_STUDIO_BUILD_DIR%"
 if not exist "%OBS_STUDIO_DIR%" (
 	git clone --recursive -b %OBS_STUDIO_VERSION% --single-branch https://github.com/obsproject/obs-studio.git "obs-studio-%OBS_STUDIO_VERSION%"
 )
-:: download obs windows deps
-if not exist "%WINDOWS_DEPS_DIR%" (
-	if not exist "%WINDOWS_DEPS_VERSION%.zip" (
-		curl -kLO "https://github.com/obsproject/obs-deps/releases/download/%OBS_DEPS_RELEASE_DATE%/windows-deps-%OBS_DEPS_RELEASE_DATE%-x64.zip" -f --retry 5 -C -
-	)
-	7z x "windows-deps-%OBS_DEPS_RELEASE_DATE%-x64.zip" -o"%WINDOWS_DEPS_DIR%"
-)
 :: download the official release of obs studio (to copy signed win-capture)
 if not exist "%OBS_STUDIO_RELEASE_DIR%" (
-	if not exist "OBS-Studio-29.1.1.zip" (
-		curl -kLO "https://github.com/obsproject/obs-studio/releases/download/%OBS_STUDIO_VERSION%/OBS-Studio-29.1.1.zip" -f --retry 5 -C -
+	if not exist "OBS-Studio-%OBS_STUDIO_VERSION%.zip" (
+		curl -kLO "https://github.com/obsproject/obs-studio/releases/download/%OBS_STUDIO_VERSION%/OBS-Studio-%OBS_STUDIO_VERSION%.zip" -f --retry 5 -C -
 	)
-	7z x "OBS-Studio-29.1.1.zip" -o"%OBS_STUDIO_RELEASE_DIR%"
+	7z x "OBS-Studio-%OBS_STUDIO_VERSION%.zip" -o"%OBS_STUDIO_RELEASE_DIR%"
 )
 
 :: clean build folder if it exists from previous attempt
-rmdir /s /q %OBS_INSTALL_PREFIX% 2>NUL
+rmdir /s /q %OBS_INSTALL_PREFIX%"" 2>NUL
 mkdir "%OBS_INSTALL_PREFIX%" 2>NUL
 cd "%OBS_STUDIO_DIR%"
 
 :: build for Win64
-cmake -S . -B "%OBS_INSTALL_PREFIX%" -G "Visual Studio 17 2022" ^
-	-DCMAKE_GENERATOR_PLATFORM="x64" ^
-	-DCMAKE_SYSTEM_VERSION="10.0.18363.657" ^
-	-DCMAKE_PREFIX_PATH:PATH="%WINDOWS_DEPS_DIR%" ^
-	-DCMAKE_INSTALL_PREFIX="%OBS_INSTALL_PREFIX%\install" ^
-	-DENABLE_BROWSER=OFF ^
-	-DENABLE_VLC=OFF ^
-	-DENABLE_UI=OFF ^
-	-DENABLE_VST=OFF ^
-	-DENABLE_SCRIPTING=OFF ^
-	-DCOPIED_DEPENDENCIES=OFF ^
-    -DCOPY_DEPENDENCIES=ON ^
-	-DBUILD_FOR_DISTRIBUTION=ON
+cmake -S . -B "%OBS_INSTALL_PREFIX%" --preset windows-x64 ^
+	-DENABLE_BROWSER:BOOL=OFF ^
+	-DENABLE_VLC:BOOL=OFF ^
+	-DENABLE_UI:BOOL=OFF ^
+	-DENABLE_VST:BOOL=OFF ^
+	-DENABLE_SCRIPTING:BOOL=OFF ^
+	-DCOPIED_DEPENDENCIES:BOOL=OFF ^
+    -DCOPY_DEPENDENCIES:BOOL=ON ^
+	-DBUILD_FOR_DISTRIBUTION:BOOL=ON
 
 cmake --build "%OBS_INSTALL_PREFIX%" --config Release
 
-:: copy rest of the required dependencies ourselves, because cmake doesn't automatically do them for some reason?
-:: this task essentially replicates (some of) CopyMSVCBins.cmake
-xcopy "%WINDOWS_DEPS_DIR%\bin" "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\" /E /Y /I
-:: copy plugins & data to bin directory to make loadModule work
-xcopy "%OBS_INSTALL_PREFIX%\rundir\Release\obs-plugins" "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\obs-plugins\" /E /Y /I
-xcopy "%OBS_INSTALL_PREFIX%\rundir\Release\data" "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\data\" /E /Y /I
-:: copy win-capture from official release to our build (because we need signed files for better compatibility)
-xcopy "%OBS_STUDIO_RELEASE_DIR%\data\obs-plugins\win-capture" "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\data\obs-plugins\win-capture\" /E /Y /I
+cd "%BASE_DIR%"
 
-cd %BASE_DIR%
+if not exist "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\obs.dll" (
+	echo "Fatal error: Failed to find obs.dll"
+	exit /b 1
+)
+
+setlocal enabledelayedexpansion
+
+set "deps_dir=%OBS_STUDIO_DIR%\.deps"
+set "newest_date="
+set "newest_dir="
+
+:: Iterate through directories in .deps/ to fetch the latest x64 deps
+for /f "tokens=*" %%a in ('dir /b /ad "%deps_dir%\obs-deps-*" ^| findstr /r /c:"obs-deps-[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]-x64$"') do (
+    :: Extract the date part from the directory name
+    set "dir_name=%%~nxa"
+    set "date_part=!dir_name:~9,10!"
+
+    :: Compare the date with the current newest one
+    if "!date_part!" gtr "!newest_date!" (
+        set "newest_date=!date_part!"
+        set "newest_dir=%%a"
+    )
+)
+
+:: Set the variable to the newest directory
+set "WINDOWS_DEPS_DIR=%deps_dir%\%newest_dir%"
+
+:: copy rest of the required dependencies ourselves, because cmake doesn't automatically do them for some reason?
+robocopy "%WINDOWS_DEPS_DIR%\bin " "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\ " /E /IS /IT /R:0 /W:0 /XD "%WINDOWS_DEPS_DIR%\bin\Lib" /XF *.pdb || IF %ERRORLEVEL% GEQ 8 goto:copy_error
+:: copy plugins & data to bin directory to make loadModule work
+robocopy "%OBS_INSTALL_PREFIX%\rundir\Release\obs-plugins " "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\obs-plugins\ " /E /IS /IT /R:0 /W:0 /XF *.ini *.pdb || IF %ERRORLEVEL% GEQ 8 goto:copy_error
+robocopy "%OBS_INSTALL_PREFIX%\rundir\Release\data " "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\data\ " /E /IS /IT /R:0 /W:0 /XF *.ini *.pdb || IF %ERRORLEVEL% GEQ 8 goto:copy_error
+:: copy win-capture from official release to our build (because we need signed files for better compatibility)
+robocopy "%OBS_STUDIO_RELEASE_DIR%\data\obs-plugins\win-capture " "%OBS_INSTALL_PREFIX%\rundir\Release\bin\64bit\data\obs-plugins\win-capture\ " /E /IS /IT /R:0 /W:0 /XF *.ini *.pdb || IF %ERRORLEVEL% GEQ 8 goto:copy_error
+
+echo "OBS build completed successfully"
+exit /b 0
+
+:copy_error
+echo "Fatal error: Failed to copy files (code: %ERRORLEVEL%)"
+exit /B 1
