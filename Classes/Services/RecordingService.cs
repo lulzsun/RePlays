cb69c4fb6b6c30f432cb1e167614378a67fc247d
sonicv6 <masterssword007@yahoo.co.uk>
2024-11-15 14:13:29 +0000

    refactor: LibObsRecorder refactor
    Made LibObsRecorder a bit neater by separating StartRecording() into multiple methods.

--- Classes/Services/RecordingService.cs
+++ Classes/Services/RecordingService.cs
@@ -1,8 +1,10 @@
 ﻿using RePlays.Recorders;
 using RePlays.Utils;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Timers;
+using static RePlays.Utils.Functions;
 using Timer = System.Timers.Timer;
 
 
@@ -21,12 +23,17 @@ namespace RePlays.Services {
         private static bool IsRestarting { get; set; }
         public static bool GameInFocus { get; set; }
 
+        const int retryInterval = 2000; // 2 second
+        const int maxRetryAttempts = 20; // 30 retries
+
         public class Session {
             public int Pid { get; internal set; }
             public nint WindowHandle { get; internal set; }
             public string GameTitle { get; internal set; }
             public string Exe { get; internal set; }
             public bool ForceDisplayCapture { get; internal set; }
+            public string videoSavePath { get; internal set; }
+
             public Session(int _Pid, nint _WindowHandle, string _GameTitle, string _Exe = null, bool _ForceDisplayCapture = false) {
                 Pid = _Pid;
                 WindowHandle = _WindowHandle;
@@ -48,6 +55,8 @@ namespace RePlays.Services {
             ActiveRecorder = new LibObsRecorder();
             Logger.WriteLine("Creating a new ActiveRecorder");
             await Task.Run(() => ActiveRecorder.Start());
+            //Update user settings
+            WebMessage.SendMessage(GetUserSettings());
             await Task.Run(() => DetectionService.CheckTopLevelWindows());
         }
 
@@ -59,19 +68,69 @@ namespace RePlays.Services {
             return currentSession;
         }
 
+        private static async Task<bool> GetGoodWindowHandle() {
+            int retryAttempt = 0;
+            while ((DetectionService.HasBadWordInClassName(currentSession.WindowHandle) || currentSession.WindowHandle == IntPtr.Zero) && retryAttempt < maxRetryAttempts) {
+                Logger.WriteLine($"Waiting to retrieve process handle... retry attempt #{retryAttempt}");
+                await Task.Delay(retryInterval);
+                retryAttempt++;
+                // alternate on retry attempts, one or the other might get us a better handle
+                currentSession.WindowHandle = WindowService.GetWindowHandleByProcessId(currentSession.Pid, retryAttempt % 2 == 1);
+            }
+            if (retryAttempt >= maxRetryAttempts) {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool HandleManualRecord() {
+            if (WindowService.GetForegroundWindow(out int processId, out nint hwid)) {
+                if (processId != 0 || hwid != 0) {
+                    WindowService.GetExecutablePathFromProcessId(processId, out string executablePath);
+                    DetectionService.AutoDetectGame(processId, executablePath, hwid, autoRecord: false);
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        private static bool SetSessionDetails() {
+            string dir = Path.Join(GetPlaysFolder(), "/" + MakeValidFolderNameSimple(currentSession.GameTitle) + "/");
+            try {
+                Directory.CreateDirectory(dir);
+            }
+            catch (Exception e) {
+                WebMessage.DisplayModal(string.Format("Unable to create folder {0}. Do you have permission to create it?", dir), "Recording Error", "warning");
+                Logger.WriteLine(e.ToString());
+                return false;
+            }
+            string videoNameTimeStamp = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+
+            FileFormat currentFileFormat = SettingsService.Settings.captureSettings.fileFormat ?? (new FileFormat("mp4", "MP4 (.mp4)", true));
+            Logger.WriteLine($"Output file format: " + currentFileFormat.ToString());
+            currentSession.videoSavePath = Path.Join(dir, videoNameTimeStamp + "-ses." + currentFileFormat.GetFileExtension());
+            return true;
+        }
+
         //[STAThread]
-        public static async void StartRecording() {
+        public static async void StartRecording(bool manual) {
             if (IsRecording || IsPreRecording) {
                 Logger.WriteLine($"Cannot start recording, already recording [{currentSession.Pid}][{currentSession.GameTitle}]");
                 return;
             }
 
             IsPreRecording = true;
-            bool result = await ActiveRecorder.StartRecording();
-            Logger.WriteLine("Start Success: " + result.ToString());
-            Logger.WriteLine("Still allowed to record: " + (!IsRecording && result).ToString());
+            bool result = true;
+
+            if (manual) result = HandleManualRecord();
+            if (result) result = await GetGoodWindowHandle();
+            if (result) result = SetSessionDetails();
+            if (result) result = await ActiveRecorder.StartRecording();
+            Logger.WriteLine("Start Success: " + result);
+            Logger.WriteLine("Still allowed to record: " + (!IsRecording && result));
             if (!IsRecording && result) {
-                Logger.WriteLine("Current Session PID: " + currentSession.Pid.ToString());
+                Logger.WriteLine("Current Session PID: " + currentSession.Pid);
 
                 startTime = DateTime.Now;
                 recordingTimer.Elapsed += OnTimedEvent;
@@ -133,7 +192,9 @@ namespace RePlays.Services {
             IsRestarting = true;
 
             bool stopResult = await ActiveRecorder.StopRecording();
-            bool startResult = await ActiveRecorder.StartRecording();
+            bool newSession = SetSessionDetails();
+            bool startResult = false;
+            if (newSession) startResult = await ActiveRecorder.StartRecording();
 
             if (stopResult && startResult) {
                 Logger.WriteLine("Recording restart successful");
