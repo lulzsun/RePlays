03e8ca7d9d902f7160a891948f064e9c5e99c8f6
Jimmy Quach <jminquach@gmail.com>
2025-12-08 20:56:28 -0800

    fix: Use regex to correctly parse timespan
    Last commit did not properly solve the issue, probably due to substring magic numbers

--- Classes/Utils/Helpers.cs
+++ Classes/Utils/Helpers.cs
@@ -556,7 +556,11 @@ namespace RePlays.Utils {
             });
             process.ErrorDataReceived += new DataReceivedEventHandler((s, e) => {
                 if (e.Data != null && e.Data.Contains("frame=") && e.Data.Contains("speed=") && !e.Data.Contains("Lsize=")) {
-                    string timeString = e.Data.Trim().Substring(48, 11).TrimStart('=');
+                    var match = Regex.Match(e.Data, @"time=(\d{2}:\d{2}:\d{2}(?:\.\d+)?)");
+                    if (!match.Success) {
+                        return;
+                    }
+                    string timeString = match.Groups[1].Value;
                     if (TimeSpan.TryParse(timeString, out TimeSpan parsedTime)) {
                         WebMessage.DisplayToast(uuid, game, "Creating clip", "none", Convert.ToInt32(parsedTime.TotalSeconds) + progress, totalRenderTime);
                     }
